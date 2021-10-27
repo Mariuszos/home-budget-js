e67deae248fc7c80c493441c688d03ee6f1ff3c7
Mariusz Martynowicz <mariusz.martynowicz@gmail.com>
2021-10-27 16:24:50 +0200

    next commit

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const btn = document.createElement("button");
-btn.textContent = "Edytuj";
-btn.classList.add("edit");
-document.getElementById("myList").appendChild(btn);
+const incomesName = document.getElementById("#addIncomesName");
+const incomesValue = document.getElementById("#addIncomesValue");
+const expensesName = document.getElementById("#addExpensesName");
+const expensesValue = document.getElementById("#addExpensesValue");
+
+const sumIncomes = 0;
+for (let i = 0; i < incomesValue.clientHeight; i++) {
+  return (sumIncomes = sumIncomes + incomesValue[i]);
+}
+
+const sumExpenses = 0;
+for (let i = 0; i < expensesValue.clientHeight; i++) {
+  return (sumExpenses = sumExpenses + expensesValue[i]);
+}
+
+const cashBalance = `${sumIncomes} - ${sumExpenses}`;
+document.getElementById(myCash).appendChild(cashBalance);
+
+// const btn = document.createElement("button");
+// btn.textContent = "Edytuj";
+// btn.classList.add("edit");
+// document.getElementById("myList").appendChild(btn);
